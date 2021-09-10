eea1fa0b64e910d67a03793f24857d9b47ce7f6f
Renato Soma <renatoys08@gmail.com>
2021-09-09 21:38:05 -0300

    Fix BG96 pin assignments
    Initialize BG96

--- src/main.c
+++ src/main.c
@@ -34,6 +34,26 @@
 #error "Unsupported board: uart0 devicetree node is not defined"
 #endif
 
+#define GPIO0_NODE DT_NODELABEL(gpio0)
+#if DT_NODE_HAS_STATUS(GPIO0_NODE, okay)
+#define MDM_STATUS_PIN    31
+#define MDM_RST_PIN       28
+#define MDM_PWR_PIN       2
+#define MDM_W_DISABLE_PIN 29
+#define MDM_DTR_PIN       26
+#define MDM_AP_RDY_PIN    30
+#define MDM_PSM_PIN       3
+#else
+#error "Unsupported board: gpio0 devicetree node is not defined"
+#endif
+
+#define GPIO1_NODE DT_NODELABEL(gpio1)
+#if DT_NODE_HAS_STATUS(GPIO1_NODE, okay)
+#define MDM_3V8_PIN       9
+#else
+#error "Unsupported board: gpio1 devicetree node is not defined"
+#endif
+
 /* Static methods */
 static const struct device *init_led() {
 	int ret;
@@ -56,23 +76,88 @@ static const struct device *init_led() {
 static const struct device *init_shtc3() {
 	const struct device *shtc3_dev = device_get_binding(DT_LABEL(SHTC3_NODE));
 
-	if (shtc3_dev == NULL) {
-		printk("SHTC3 not found\n");
-		return NULL;
-	}
-
 	return shtc3_dev;
 }
 
 static const struct device *init_uart() {
 	const struct device *uart0_dev = device_get_binding(DT_LABEL(UART0_NODE));
 
-	if (uart0_dev == NULL) {
-		printk("UART0 not found\n");
+	return uart0_dev;
+}
+
+void init_gpio1() {
+	int ret;
+	const struct device *gpio_dev = device_get_binding(DT_LABEL(GPIO1_NODE));
+
+	if (gpio_dev == NULL) {
+		printk("GPIO1 not found");
+	}
+
+	/* 3V8 */
+	ret = gpio_pin_configure(gpio_dev, MDM_3V8_PIN, GPIO_OUTPUT_ACTIVE);
+	if (ret < 0) {
+		printk("Error configuring MDM_3V8_PIN: %d", ret);
+	}
+
+	gpio_pin_set(gpio_dev, MDM_3V8_PIN, 1);
+}
+
+static const struct device *init_gpio0() {
+	int ret;
+	const struct device *gpio_dev = device_get_binding(DT_LABEL(GPIO0_NODE));
+
+	if (gpio_dev == NULL) {
+		printk("GPIO0 not found");
 		return NULL;
 	}
 
-	return uart0_dev;
+	/* status pin */
+	ret = gpio_pin_configure(gpio_dev, MDM_STATUS_PIN, GPIO_INPUT);
+	if (ret < 0) {
+		printk("Error configuring MDM_STATUS_PIN: %d", ret);
+	}
+
+	/* dtr pin */
+	ret = gpio_pin_configure(gpio_dev, MDM_DTR_PIN, GPIO_INPUT);
+	if (ret < 0) {
+		printk("Error configuring MDM_DTR_PIN: %d", ret);
+	}
+
+	/* ap ready pin */
+	ret = gpio_pin_configure(gpio_dev, MDM_AP_RDY_PIN, GPIO_INPUT);
+	if (ret < 0) {
+		printk("Error configuring MDM_AP_RDY_PIN: %d", ret);
+	}
+
+	/* psm pin */
+	ret = gpio_pin_configure(gpio_dev, MDM_PSM_PIN, GPIO_INPUT);
+	if (ret < 0) {
+		printk("Error configuring MDM_PSM_PIN: %d", ret);
+	}
+
+	/* reset pin */
+	ret = gpio_pin_configure(gpio_dev, MDM_RST_PIN, GPIO_OUTPUT_INACTIVE);
+	if (ret < 0) {
+		printk("Error configuring MDM_RST_PIN: %d", ret);
+	}
+
+	/* power pin */
+	ret = gpio_pin_configure(gpio_dev, MDM_PWR_PIN, GPIO_OUTPUT_ACTIVE);
+	if (ret < 0) {
+		printk("Error configuring MDM_PWR_PIN: %d", ret);
+	}
+
+	/* w disable pin */
+	ret = gpio_pin_configure(gpio_dev, MDM_W_DISABLE_PIN, GPIO_OUTPUT_ACTIVE);
+	if (ret < 0) {
+		printk("Error configuring MDM_W_DISABLE_PIN: %d", ret);
+	}
+
+	gpio_pin_set(gpio_dev, MDM_RST_PIN, 0);
+	gpio_pin_set(gpio_dev, MDM_PWR_PIN, 1);
+	gpio_pin_set(gpio_dev, MDM_W_DISABLE_PIN, 1);
+
+	return gpio_dev;
 }
 
 static void shtc3_sensor_read(const struct device *shtc3_dev) {
@@ -105,7 +190,9 @@ void main(void)
 {
 	const struct device *led_dev = init_led();
 	const struct device *uart_dev = init_uart();
+	const struct device *gpio0_dev = init_gpio0();
 	const struct device *shtc3_dev = init_shtc3();
+	init_gpio1();
 
 	if (led_dev == NULL) {
 		printk("Led not found, stopping...\n");
@@ -119,6 +206,11 @@ void main(void)
 
 	if (uart_dev == NULL) {
 		printk("UART0 not found, stopping...\n");
+		return;
+	}
+
+	if (gpio0_dev == NULL) {
+		printk("GPIO_0 not found, stopping...\n");
 		return;
 	}
 
