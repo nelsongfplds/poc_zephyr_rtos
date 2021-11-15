6552e7a4d6de9406202ebf4bde987a2a40d288d4
Renato Soma <renatoys08@gmail.com>
2021-11-14 22:50:25 -0300

    Reworking logs

--- src/main.c
+++ src/main.c
@@ -24,19 +24,16 @@ void main(void)
 	int reps = 0;
 	char rsp[100];
 	printk("Begin main loop\n");
-	while (reps < 1) {
+	while (reps <= 1) {
+		printk("Iteration: %d\n", reps);
 		shtc3_sensor_read();
 
 		send_at_command(send_buffer, strlen(send_buffer), rsp);
 		printk("main, returned: %s\n", rsp);
+		k_msleep(10000);
 
 		reps++;
 	}
 
-	k_msleep(10000);
-	memset(send_buffer, 0, RING_BUFFER_SIZE);
-	memcpy(send_buffer, "AT+GMI", 6);
-	/* memcpy(send_buffer, "AT+QPOWD=0\r", 11); */
-	send_at_command(send_buffer, strlen(send_buffer), rsp);
-	printk("Exiting main...");
+	printk("Exiting main\n");
 }
