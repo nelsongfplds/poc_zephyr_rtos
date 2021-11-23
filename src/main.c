6a0f0a8b170982ec1def32991141f5ee7697d4d7
Renato Soma <renatoys08@gmail.com>
2021-11-23 17:42:43 -0300

    Begin payload creation

--- src/main.c
+++ src/main.c
@@ -1,34 +1,60 @@
 #include "main.h"
 
+#define PAYLOAD_MAX_SIZE 600
+
+void set_payload(char *buffer,
+		 char *imei,
+		 char *vbatt) {
+	snprintk(buffer, PAYLOAD_MAX_SIZE,
+		 "{"
+			 "\"id_can\":\"%s\","
+			 "\"id_sku\":\"%s\","
+			 "\"id_device\":\"%s\","
+			 "\"bat_val\":\"%s\""
+		 "}",
+	"dev0",
+	"sku_dev_0",
+	imei,
+	vbatt);
+}
+
 void main(void)
 {
-	if (init_board_sensors() == false) {
-		printk("Failed sensor initialization, stopping...\n");
-		return;
-	}
+	printk("Hello\n");
 
-	if (init_bg96() == false) {
-		printk("Failed BG96 initialization, stopping...\n");
-		return;
-	}
+	char *buffer = k_malloc(PAYLOAD_MAX_SIZE*sizeof(char));
+	/* char buffer[PAYLOAD_MAX_SIZE]; */
+	memset(buffer, 0, PAYLOAD_MAX_SIZE);
+	set_payload(buffer, "abc123456", "9100mV");
+	printk("%s\n", buffer);
+	free(buffer);
+	/* if (init_board_sensors() == false) { */
+	/* 	printk("Failed sensor initialization, stopping...\n"); */
+	/* 	return; */
+	/* } */
 
-	printk("Devices initialized!\n");
+	/* if (init_bg96() == false) { */
+	/* 	printk("Failed BG96 initialization, stopping...\n"); */
+	/* 	return; */
+	/* } */
 
-	k_msleep(SLEEP_TIME_MS*2);
+	/* printk("Devices initialized!\n"); */
 
-	int ret = server_connect();
-	if (ret) {
-		printk("Connected\n");
-		send_payload("testetesteteste", strlen("testetesteteste"));
-	} else {
-		printk("Not connected\n");
-	}
+	/* k_msleep(SLEEP_TIME_MS*2); */
+
+	/* int ret = server_connect(); */
+	/* if (ret) { */
+	/* 	printk("Connected\n"); */
+	/* 	send_payload("testetesteteste", strlen("testetesteteste")); */
+	/* } else { */
+	/* 	printk("Not connected\n"); */
+	/* } */
 
 	/* memset(rsp, 0, 100); */
 	/* printk("\n\nGet IMEI\n"); */
 	/* send_at_command("AT+GSN", strlen("AT+GSN"), rsp); */
 	/* 	printk("main, returned: %s\n", rsp); */
-	k_msleep(10000);
+	/* k_msleep(10000); */
 
 	printk("Exiting main\n");
 }
